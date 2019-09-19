b6a7077619d6e98f07888f9e5d17712b9112dcc4
un-pogaz <46523284+un-pogaz@users.noreply.github.com>
2019-09-19 15:58:53 +0200

    IDpub import

---
+++ Chromatik/IO/StreamExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.IO
+{
+    static public class StreamExtension
+    {
+        static public Stream CreateClone(this Stream stream)
+        {
+            long p = stream.Position;
+            MemoryStream rslt = new MemoryStream();
+
+            rslt.Position = 0;
+            stream.Position = 0;
+            stream.CopyTo(rslt);
+            rslt.Position = 0;
+            stream.Position = p;
+
+            return rslt;
+        }
+    }
+}
---
+++ Chromatik/Text/UTF8SansBomEncoding.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Text
+{
+    public class UTF8SansBomEncoding : UTF8Encoding
+    {
+        /// <summary>
+        /// <see cref="Encoding"/> pour de l'UTF8 sans BOM
+        /// </summary>
+        new static public UTF8SansBomEncoding Default { get; } = new UTF8SansBomEncoding();
+
+        /// <summary>
+        /// <see cref="Encoding"/> pour de l'UTF8 sans BOM
+        /// </summary>
+        public UTF8SansBomEncoding() : this(true)
+        { }
+        /// <summary>
+        /// <see cref="Encoding"/> pour de l'UTF8 sans BOM
+        /// </summary>
+        /// <param name="throwOnInvalidBytes">true pour lever une exception quand un encodage non valide est détecté ; sinon, false.</param>
+        public UTF8SansBomEncoding(bool throwOnInvalidBytes) : base(false, throwOnInvalidBytes)
+        {
+        }
+    }
+}
