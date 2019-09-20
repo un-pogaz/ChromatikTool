1bb3167fe05551e3c330575e71a53aa50face86c
un-pogaz <46523284+un-pogaz@users.noreply.github.com>
2019-09-20 10:25:46 +0200

    XmlRacine

---
+++ Chromatik/Xml/XmlRacine.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Xml
+{
+    public class XmlRacine : XmlElement
+    {
+        public XmlRacine(string localName) : this(localName, "")
+        { }
+        public XmlRacine(string localName, string namespaceURI) : base("", localName, namespaceURI, new XmlDocument())
+        { }
+
+        static public XmlRacine Create(string name)
+        {
+            return Create(name, "");
+        }
+        static public XmlRacine Create(string name, XmlNamespace ns)
+        {
+            return Create(name, ns.URI);
+        }
+        static public XmlRacine Create(string name, string namespaceURI)
+        {
+            return new XmlRacine(name, namespaceURI);
+        }
+    }
+}
