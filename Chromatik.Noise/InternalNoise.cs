1a918cc9f4634b6f472a9c601c50c0abf4048d6f
un-pogaz <46523284+un-pogaz@users.noreply.github.com>
2019-09-30 12:28:17 +0200

    Chromatik.Noise

---
+++ Chromatik.Noise/InternalNoise.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chromatik.Noise
+{
+    public enum NoiseRange
+    {
+        Byte = 256,
+        ZeroToOne = 1,
+        nOneToOne = 2,
+        ZeroToHundred = 100,
+        ZeroToThousand = 1000,
+    }
+
+    internal static class InternalNoise
+    {
+        internal static float ToRange(float input, NoiseRange range)
+        {
+            switch (range)
+            {
+                case NoiseRange.Byte:
+                    return input * 128 + 128;
+                case NoiseRange.ZeroToOne:
+                    return input * 0.5f + 1;
+                case NoiseRange.ZeroToHundred:
+                    return input * 50 + 100;
+                case NoiseRange.ZeroToThousand:
+                    return input * 500 + 1000;
+                default: // SimplexNoiseRange.nOneToOne
+                    return input;
+            }
+        }
+    }
+}
