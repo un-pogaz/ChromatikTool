8ae1b7c540f4cbbeef276be1e691d883104210d7
un-pogaz <46523284+un-pogaz@users.noreply.github.com>
2019-12-05 17:35:00 +0100

    Enigma update

--- Chromatik.RAmen/Enigma/PlugBoard.cs
+++ Chromatik.RAmen/Enigma/PlugBoard.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,56 +13,12 @@ namespace Chromatik.Cryptography.Enigma
 		public PlugBoardException(string message) : base(message) { }
 	}
 
-	public class PlugBoard : IReadOnlyDictionary<char, char>, IEnumerable<KeyValuePair<char, char>>, System.Collections.IEnumerable
+	public class PlugBoard : ReadOnlyDictionary<char, char>
     {
 		public PlugBoard() : this(new Dictionary<char, char>())
 		{ }
-        public PlugBoard(IDictionary<char, char> dictionary)
-        {
-            plugs = new Dictionary<char, char>(dictionary);
-        }
-
-        private Dictionary<char, char> plugs = new Dictionary<char, char>();
-
-
-        public char this[char input]
-        {
-            get { return plugs[input]; }
-        }
-        public IEnumerable<char> Keys
-        {
-            get { return plugs.Keys; }
-        }
-        public IEnumerable<char> Values
-        {
-            get { return plugs.Values; }
-        }
-        public bool ContainsKey(char key)
-        {
-            return plugs.ContainsKey(key);
-        }
-        public bool ContainsValue(char value)
-        {
-            return plugs.ContainsValue(value);
-        }
-        public bool TryGetValue(char key, out char value)
-        {
-            return plugs.TryGetValue(key, out value);
-        }
-        public int Count
-        {
-            get { return plugs.Count; }
-        }
-
-        public System.Collections.Generic.IEnumerator<KeyValuePair<char, char>> GetEnumerator()
-        {
-            return plugs.GetEnumerator();
-        }
-        System.Collections.IEnumerator GetEnumerator()
-        {
-            return plugs.GetEnumerator();
-        }
-
+        public PlugBoard(IDictionary<char, char> dictionary) : base(dictionary)
+        { }
         /// <summary>
         /// Process the input character.
         /// </summary>
@@ -70,7 +27,7 @@ namespace Chromatik.Cryptography.Enigma
         public char Process(char input)
 		{
 			char output;
-			if (plugs.TryGetValue(input, out output))
+			if (TryGetValue(input, out output))
 				return output;
 			return input;
 		}
@@ -82,31 +39,13 @@ namespace Chromatik.Cryptography.Enigma
 		/// <param name="to"></param>
 		public void AddPlug(char from, char to)
 		{
-			var workingFrom = from;
-			if (char.IsLower(workingFrom))
-			{
-				workingFrom = char.ToUpper(workingFrom);
-			}
+			if (ContainsKey(from))
+				throw new PlugBoardException("Already mapped {0} <=> {1}".Format(from, Dictionary[from]));
+			if (ContainsKey(to))
+				throw new PlugBoardException("Already mapped {0} <=> {1}".Format(Dictionary[to], to));
 
-			var workingTo = to;
-			if (char.IsLower(workingTo))
-			{
-				workingTo = char.ToUpper(workingTo);
-			}
-			
-			if (plugs.ContainsKey(from))
-			{
-				var existingTo = plugs[from];
-				throw new PlugBoardException("Already mapped {0} <=> {1}".Format(from, existingTo));
-			}
-			if (plugs.ContainsKey(to))
-			{
-				var existingFrom = plugs[to];
-				throw new PlugBoardException("Already mapped {0} <=> {1}".Format(existingFrom, to));
-			}
-
-            plugs.Add(workingFrom, workingTo);
-            plugs.Add(workingTo, workingFrom);
+            Dictionary.Add(from, to);
+            Dictionary.Add(to, from);
 		}
 
         /// <summary>
@@ -115,20 +54,20 @@ namespace Chromatik.Cryptography.Enigma
         /// <param name="plug"></param>
         public void RemovePlug(char plug)
 		{
-			if (plugs.ContainsKey(plug))
+			if (ContainsKey(plug))
             {
-                char to = plugs[plug];
-                plugs.Remove(plug);
-                plugs.Remove(to);
+                char to = Dictionary[plug];
+                Dictionary.Remove(plug);
+                Dictionary.Remove(to);
             }
         }
-
+        /// <summary></summary>
 		public override string ToString()
 		{
-			if(!Plugs.Any())
-				return "None";
-			else
-				return string.Join(", ", this.Plugs.Select(_ => "{0}-{1}".Format(_.Key, _.Value)));
+            if (!Dictionary.Any())
+                return "None";
+            else
+                return Dictionary.Select(pair => "{0}-{1}".Format(pair.Key, pair.Value)).Join(", ");
 		}
 	}
 }
