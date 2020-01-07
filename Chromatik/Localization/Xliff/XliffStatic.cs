f95579a1fc7252e1521da34a9fecdd88859e6ae0
un-pogaz <46523284+un-pogaz@users.noreply.github.com>
2020-01-07 11:18:13 +0100

    QtTranslation done, Start work Xliff

---
+++ Chromatik/Localization/Xliff/XliffStatic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Globalization.Localization
+{
+    internal static class XliffStatic
+    {
+        static internal bool XmlSpacePreserve(this Xml.XmlElement node)
+        {
+            return (node.GetAttribute("xml:space") == "preserve");
+        }
+    }
+}
---
+++ Chromatik/Text/RegexMail.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Chromatik.Text
+{
+    internal static class RegexMail
+    {
+        internal const string EmailAddressPattern = @"(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))";
+
+
+        static internal bool ContainsEmailAddress(string input)
+        {
+            return input.RegexIsMatch(EmailAddressPattern, RegexOptions.IgnoreCase);
+        }
+
+        static internal bool IsEmailAddress(string input)
+        {
+            return input.RegexIsMatch("^" + EmailAddressPattern + "$", RegexOptions.IgnoreCase);
+        }
+    }
+}
